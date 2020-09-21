a692b3ca5833fdd400ef0617a4cdbeae68c7f1c9
TerrenceBell <terrencekbell@gmail.com>
2020-09-21 13:49:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app"> 
+      <h1>Covid 19 tracker!</h1>
+
+      {/* Header */}
+      {/* Title + Drop down */}
+
+      {/* info boxes  */}
+      {/* info boxes  */}
+      {/* info boxes  */}
+
+
+      
+    </div>
+  );
+}
+
+export default App;
