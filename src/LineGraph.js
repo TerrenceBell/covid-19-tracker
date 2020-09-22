992e62fa87341ddd9fce286002342e5df317c150
TerrenceBell <terrencekbell@gmail.com>
2020-09-21 23:25:49 -0500

    finished line graph functionality

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Line } from 'react-chartjs-2';
+import numeral from "numeral";
 
 const options = { 
     legend: { 
@@ -20,10 +21,33 @@ const options = {
             },
         },
     },
+    scales: { 
+        xAxes: [ 
+            { 
+                type: "time", 
+                time: { 
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
+                },
+            },
+        ],
+        yAxes: [ 
+           { 
+            gridLines: { 
+                display: false,
+            },
+            ticks: { 
+                callback: function (value, index, values) { 
+                    return numeral(value).format("0a")
+                }
+            }
+          } 
+        ]
+    }
 }
 //tooltips are to be able to hover over the graph and see the value at that specific point
 
-function LineGraph() {
+function LineGraph({ casesType = 'cases'}) {
     const [data, setData] = useState({});
     //API call for last 120 days of data  "https://disease.sh/v3/covid-19/historical/all?lastdays=120"
 
@@ -46,24 +70,37 @@ function LineGraph() {
     }
 
     useEffect(() => {
-        fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        const fetchData = async () => { 
+         await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then(resp => resp.json())
         .then(data => { 
             console.log(data)
             const chartData = buildChartData(data)
             setData(chartData)
         })
-        
-    }, []);
+        }
+        fetchData()      
+    }, [casesType]);
 
     
     
     return (
         <div>
             <h1>some graph here</h1>
-           <Line  data={{ 
-               datasets: [{data:data}]
-           }} /> 
+            
+            {data?.length > 0 && ( 
+                <Line  
+                options={options}
+                data={{ 
+                    datasets: [{
+                            backgroundColor: "rgba(204, 16, 52, 0.5)",
+                            borderColor: "#CC1034",
+                            data:data,
+                        }]
+                }} /> 
+
+            )}
+                
         </div>
     )
 }
