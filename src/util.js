33fc2c66c6b42bea72b28617a3d44bd5fa51ffe5
TerrenceBell <terrencekbell@gmail.com>
2020-09-22 14:58:13 -0500

    added red circles that vary based on the number of cases in an area

--- src/util.js
+++ src/util.js
@@ -50,7 +50,15 @@ export const showDataOnMap =(data, casesType='cases') => (
             //multiply by multiplier 
         }
         > 
-            <Popup> </Popup>
+            <Popup> 
+                <div className="info-container"> 
+                    <div className="info-flag" style={{backgroundImage: `url(${country.countryInfo.flag}) `}} />
+                    <div className="info-name">{country.country}</div>
+                    <div className="info-cases">Cases: {numeral(country.cases).format("0,0")}</div>
+                    <div className="info-recovered">Recovered: {numeral(country.recovered).format("0,0")}</div>
+                    <div className="info-deaths">Deaths: {numeral(country.deaths).format("0.0")}</div>
+                </div>
+            </Popup>
         </Circle>
     ))
 )
